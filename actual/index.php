52bfa985849e7e9015653913d0fc6f4d586ce395
Sam Griffith <sammgriff@gmail.com>
2014-11-13 00:27:24 -0500

    Add jQuery to head. Change class to id on "about" elements. Change waves SVG footer. Add js/scripts.js. Have working "About" button. Change Header and Footer <h2> styling. Add styling for About windows and its active state.

--- actual/index.php
+++ actual/index.php
@@ -7,6 +7,8 @@
 	<link rel="stylesheet" href="styles/styles.css">
 	<script	src="http://maps.googleapis.com/maps/api/js?key=AIzaSyArNFTlstSX6IZDDmgTJI5YZ7mAnST6glc&sensor=false"></script>
 	<?php include "includes/map.php" ?>
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+	<script type="text/javascript" src="js/scripts.js"></script>
 
 </head>
 
@@ -17,10 +19,14 @@
 
 	<div id="googleMap"></div>
 
+	<div id="aboutWindow">
+		<h2>About<h2>
+		<p>Check me out</p>
+	</div>
+
 	<div id="footer">
-		<a href=#><h2 class="about" >About</h2></a>
+		<a href=#><h2 id="about">About</h2></a>
 		<img src="img/waves.svg" alt="Waves">
-		
 	</div>
 
 </body>
---
+++ actual/js/scripts.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+
+	$("#about").click(function() {
+	   $("#aboutWindow").toggleClass("actived");
+	});
+
+});
