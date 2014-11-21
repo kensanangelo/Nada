318e69805610fa10cd05d3396402f17dc6170d9f
Sam Griffith <sammgriff@gmail.com>
2014-11-20 23:29:29 -0500

    Reduce Wave footer artboard. Add fonts. Change lots of CSS.

--- actual/index.php
+++ actual/index.php
@@ -19,38 +19,42 @@
 	<script type="text/javascript" src="js/data.json"></script>
 	<script type="text/javascript" src="js/markerclusterer.js"></script>
 	<script type="text/javascript" src="js/scripts.js"></script>
+	<link href='http://fonts.googleapis.com/css?family=Bubblegum+Sans' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Karla:700,400' rel='stylesheet' type='text/css'>
 
 </head>
 
 <body>
 	<div id="header">
-		<a href=#><h2 id="about">NADA maps</h2></a>
+		<a href=#><h2 id="about">Nada Maps</h2></a>
 		<form id="search" action=#>
   			<input type="search" id='searchBox' name="fishsearch" placeholder='Search Here'>
-  			<input type="submit" value="Search">
+  			<input type="submit" id='searchButton' value="Search">
 		</form>
 	</div>
 
 	<div id="googleMap"></div>
 
 	<div id="aboutWindow">
-		<div class="windowContent">
+		<div>
 			<div class="row">
 				<h3 class='col-md-6' id='resultsHeader'>Welcome to Nada Maps!</h3>
 				<button type="button" class="close pull-right" id='aboutClose'><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
 			</div>
-			<p>Are you a fish? Do you find it hard to avoid being caught by fisherman when going to your favorite lakes and rivers? Don't you wish there was a way to know where fisherman went to avoid them all together? Well, now there is!<br/>Welcome to Nada Maps! We show you the most popular fishing spots in all of New York, and where the fisherman are going for certain fish. Click on a point to learn more about it, or use the search up above.</p>
+			<div class="windowContent">
+				<p class="col-md-12">Are you a fish? Do you find it hard to avoid being caught by fisherman when going to your favorite lakes and rivers? Don't you wish there was a way to know where fisherman went to avoid them all together? Well, now there is!<br/>Welcome to Nada Maps! We show you the most popular fishing spots in all of New York, and where the fisherman are going for certain fish. Click on a point to learn more about it, or use the search up above.</p>
+			</div>
 		</div>
 	</div>
 
 	<div id="searchWindow">
-		<div class="windowContent">
+		<div>
 			<div class="row">
 				<h3 class='col-md-8' id='resultsHeader'>Search Results</h3>
 				<button type="button" class="close pull-right" id='searchClose'><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
 			</div>
 			<div id='resultsWindow'>
-				<p>No results found.</p>
+				<p class="col-md-12">No results found.</p>
 			</div>
 		</div>
 	</div>
